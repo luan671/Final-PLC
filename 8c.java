4ea20f2dacaf0aa163e171622f004fd8ebadf01c
luan671 <61252804+luan671@users.noreply.github.com>
2020-04-30 21:14:51 -0400

    Create 8c.java

---
+++ 8c.java
@@ -0,0 +1,26 @@
+public class PLC{
+
+    public static void main(String []args){
+    j= -3;
+    i= 0;
+
+while(j<0 && i<3)
+{
+
+    if(j==3 || j==2)
+
+    j--;
+
+        else if (j==0)
+
+        j+= 2;
+
+    else
+
+    j= 0;
+
+    j= 3-i;
+
+        }
+    }
+}
